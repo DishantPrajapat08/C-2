6216a7dc6e435d8e3f588bbf0db5cc6531a2f637
DishantPrajapat08 <dishantprajapat08>
2026-07-30 14:52:46 +0530

    inverted triangle

---
+++ tut2.cpp
@@ -0,0 +1,28 @@
+// Inverted triangle pattern
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the vaue of n" << endl;
+    cin >> n;
+ int num=1;
+    for (int i = 0; i < n; i++)
+    {
+        for(int m=1;m<i+1;m++)
+        {
+            cout<<"  ";
+        }
+       
+        for (int j = n; j>i+1; j--)
+        {
+            cout<<num<<" ";
+           
+        }
+        cout<<endl;
+         num+=1;
+        
+    }
+    return 0;
+}
