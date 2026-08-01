620f7f73bd46b86c119868215b28eebaba8981cc
DishantPrajapat08 <dishantprajapat08>
2026-08-01 12:18:55 +0530

    Tut6

---
+++ tut6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int fact = 1;
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        for (int i = 1; i <= n; i++)
+        {
+            fact *= i;
+        }
+    return fact;
+}
+int main()
+{
+    cout << factorial(57) << endl;
+    return 0;
+}
