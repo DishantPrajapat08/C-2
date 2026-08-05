fca379a1cf4635395989a532c0492ea760768d58
DishantPrajapat08 <dishantprajapat08>
2026-08-05 20:39:38 +0530

    binto dec

---
+++ tut9.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+
+int bintodec(int n)
+{
+    int ans=0,pow=1;
+    
+    while(n>0)
+    {
+       int rem=n%10;
+       ans+=(rem*pow);
+       n=n/10;
+       pow*=2;
+    }
+    return ans;
+}
+int main(){
+    cout<<bintodec(101010);
+    return 0;
+}
